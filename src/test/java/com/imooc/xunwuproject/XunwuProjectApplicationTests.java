329e48fc0f647b368486a32267d3ac2df418df3c
csucoderlee <csucoderlee@foxmail.com>
2021-05-05 22:28:30 +0800

    init project

---
+++ src/test/java/com/imooc/xunwuproject/XunwuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.xunwuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XunwuProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
